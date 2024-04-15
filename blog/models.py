442e30034533dfec2571d3fc6de7fa5958ea50a4
Daniel madera <danielmadera2004@gmail.com>
2024-04-15 13:41:11 -0500

    Agregar comentario en archivo models

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+#Clase para las publicaciones
 class Publicacion(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
