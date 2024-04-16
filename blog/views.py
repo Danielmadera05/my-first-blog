5cc46d71519d52afd2bc0d8fb8e04dd8bd766f05
Daniel madera <danielmadera2004@gmail.com>
2024-04-16 16:25:00 -0500

    Modificaciones para mostras posts desde base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #Realiza una consulta de posts realizado y los ordena por fecha de pulicacion
+    publicacion = Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #Se crea una variable posts que almacene el resultado de la consulta y se muestre en la pagina HTML
+    return render(request, 'blog/post_list.html', {'posts': publicacion})
